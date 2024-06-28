1924cbbfa7a8042b059eae5be441391e3752fc4f
wangqi <wangqi@xinxuan.net>
2024-06-28 18:38:25 +0800

    feature: update security for open api

--- carp-modules/carp-module-security/carp-module-security-core/src/main/java/cn/sliew/carp/module/security/core/service/SecAuthorizationService.java
+++ carp-modules/carp-module-security/carp-module-security-core/src/main/java/cn/sliew/carp/module/security/core/service/SecAuthorizationService.java
@@ -18,5 +18,103 @@
 
 package cn.sliew.carp.module.security.core.service;
 
+import cn.sliew.carp.module.security.core.service.dto.SecRoleDTO;
+import cn.sliew.carp.module.security.core.service.dto.SecUserDTO;
+import cn.sliew.carp.module.security.core.service.param.authorize.*;
+import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
+
+import java.util.List;
+
 public interface SecAuthorizationService {
+
+    // -------------------------------------------------------------------------------------------
+    // resource-web -> role
+    // -------------------------------------------------------------------------------------------
+
+    /**
+     * 查询 资源-web 绑定角色列表
+     */
+    Page<SecRoleDTO> listAuthorizedRolesByResourceWebId(SecRoleListByResourceWebParam param);
+
+    /**
+     * 查询 资源-web 未绑定角色列表
+     */
+    Page<SecRoleDTO> listUnauthorizedRolesByResourceWebId(SecRoleListByResourceWebParam param);
+
+    /**
+     * 批量为 资源-web 绑定角色
+     */
+    void authorize(SecRoleBatchAuthorizeForResourceWebParam param);
+
+    /**
+     * 批量为 资源-web 解除角色绑定
+     */
+    void unauthorize(SecRoleBatchAuthorizeForResourceWebParam param);
+
+    // -------------------------------------------------------------------------------------------
+    // role -> resource-web
+    // -------------------------------------------------------------------------------------------
+
+    /**
+     * 查询所有 资源-web 和指定角色绑定状态
+     */
+    List listResourceWebsByRoleId(SecResourceWebListByRoleParam param);
+
+    /**
+     * 批量为角色绑定 资源-web
+     */
+    void authorize(SecResourceWebBatchAuthorizeForRoleParam param);
+
+    /**
+     * 批量为角色解除 资源-web 绑定
+     */
+    void unauthorize(SecResourceWebBatchAuthorizeForRoleParam param);
+
+    // -------------------------------------------------------------------------------------------
+    // role -> user
+    // -------------------------------------------------------------------------------------------
+
+    /**
+     * 查询角色绑定用户列表
+     */
+    Page<SecUserDTO> listAuthorizedUsersByRoleId(SecUserListByRoleParam param);
+
+    /**
+     * 查询角色未绑定用户列表
+     */
+    Page<SecUserDTO> listUnauthorizedUsersByRoleId(SecUserListByRoleParam param);
+
+    /**
+     * 批量为角色绑定用户
+     */
+    void authorize(SecUserBatchAuthorizeForRoleParam param);
+
+    /**
+     * 批量为角色解除用户绑定
+     */
+    void unauthorize(SecUserBatchAuthorizeForRoleParam param);
+
+    // -------------------------------------------------------------------------------------------
+    // user -> role
+    // -------------------------------------------------------------------------------------------
+
+    /**
+     * 查询用户绑定角色列表
+     */
+    List<SecRoleDTO> listAuthorizedRolesByUserId(SecRoleListByUserParam param);
+
+    /**
+     * 查询用户未绑定角色列表
+     */
+    List<SecRoleDTO> listUnauthorizedRolesByUserId(SecRoleListByUserParam param);
+
+    /**
+     * 批量为用户绑定角色
+     */
+    void authorize(SecRoleBatchAuthorizeForUserParam param);
+
+    /**
+     * 批量为用户解除角色绑定
+     */
+    void unauthorize(SecRoleBatchAuthorizeForUserParam param);
 }
