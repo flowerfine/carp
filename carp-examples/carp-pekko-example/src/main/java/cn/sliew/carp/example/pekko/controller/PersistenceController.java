05f0d30d8082d02ea8f3c6a74d8999438494c7ba
wangqi <wangqi@xinxuan.net>
2024-08-03 15:49:26 +0800

    feature: add pekko persistence demo

--- carp-examples/carp-pekko-example/src/main/java/cn/sliew/carp/example/pekko/controller/PersistenceController.java
+++ carp-examples/carp-pekko-example/src/main/java/cn/sliew/carp/example/pekko/controller/PersistenceController.java
@@ -18,19 +18,128 @@
 
 package cn.sliew.carp.example.pekko.controller;
 
+import cn.sliew.carp.example.pekko.persistence.ShoppingCart;
+import cn.sliew.carp.example.pekko.persistence.command.ShoppingCartCommand;
+import cn.sliew.carp.example.pekko.persistence.state.ShoppingCartSummary;
+import cn.sliew.carp.framework.common.util.UUIDUtil;
+import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
+import org.apache.pekko.actor.typed.ActorRef;
 import org.apache.pekko.actor.typed.ActorSystem;
+import org.apache.pekko.actor.typed.Props;
+import org.apache.pekko.actor.typed.SpawnProtocol;
+import org.apache.pekko.actor.typed.javadsl.AskPattern;
+import org.apache.pekko.actor.typed.javadsl.Behaviors;
+import org.apache.pekko.pattern.StatusReply;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.time.Duration;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.CompletionStage;
 
 @RestController
-@RequestMapping("/api/carp/example/mongo/template")
-@Tag(name = "测试模块-MongoTemplate")
+@RequestMapping("/api/carp/example/pekko/persistence")
+@Tag(name = "测试模块-Pekko-Persistence")
 public class PersistenceController {
 
     @Autowired
-    private ActorSystem actorSystem;
+    private ActorSystem system;
 
+    private Duration timeout = Duration.ofSeconds(3);
+    private Map<String, ActorRef<ShoppingCartCommand>> shoppingCarts = new HashMap<>();
+
+    @PutMapping
+    @Operation(summary = "创建购物车", description = "创建购物车")
+    public String create() {
+        String cartId = UUIDUtil.randomUUId();
+        CompletionStage<ActorRef<ShoppingCartCommand>> completionStage =
+                AskPattern.ask(
+                        system,
+                        (ActorRef<ActorRef<ShoppingCartCommand>> replyTo) -> new SpawnProtocol.Spawn<>(Behaviors.setup(ctx -> ShoppingCart.create(cartId)), "ShoppingCartActor", Props.empty(), replyTo),
+                        timeout,
+                        system.scheduler());
+
+        completionStage.whenComplete((actorRef, throwable) -> {
+            if (throwable != null) {
+                throwable.printStackTrace();
+            } else {
+                shoppingCarts.put(cartId, actorRef);
+            }
+        });
+        return cartId;
+    }
+
+    @DeleteMapping("{cartId}")
+    @Operation(summary = "关闭购物车", description = "关闭购物车")
+    public CompletableFuture<ShoppingCartSummary> checkout(@PathVariable("cartId") String cartId) {
+        ActorRef<ShoppingCartCommand> shoppingCart = shoppingCarts.get(cartId);
+        CompletionStage<ShoppingCartSummary> completionStage =
+                AskPattern.askWithStatus(
+                        shoppingCart,
+                        (ActorRef<StatusReply<ShoppingCartSummary>> replyTo) -> new ShoppingCartCommand.Checkout(replyTo),
+                        timeout,
+                        system.scheduler());
+
+        return completionStage.toCompletableFuture();
+    }
+
+    @GetMapping("{cartId}/items")
+    @Operation(summary = "获取商品", description = "获取商品")
+    public CompletableFuture<ShoppingCartSummary> get(@PathVariable("cartId") String cartId) {
+        ActorRef<ShoppingCartCommand> shoppingCart = shoppingCarts.get(cartId);
+        CompletionStage<ShoppingCartSummary> completionStage =
+                AskPattern.ask(
+                        shoppingCart,
+                        (ActorRef<ShoppingCartSummary> replyTo) -> new ShoppingCartCommand.Get(replyTo),
+                        timeout,
+                        system.scheduler());
+
+        return completionStage.toCompletableFuture();
+    }
+
+    @PutMapping("{cartId}/items")
+    @Operation(summary = "添加商品", description = "添加商品")
+    public CompletableFuture<ShoppingCartSummary> addItem(@PathVariable("cartId") String cartId, @RequestParam("itemId") String itemId, @RequestParam("quantity") int quantity) {
+        ActorRef<ShoppingCartCommand> shoppingCart = shoppingCarts.get(cartId);
+        CompletionStage<ShoppingCartSummary> completionStage =
+                AskPattern.askWithStatus(
+                        shoppingCart,
+                        (ActorRef<StatusReply<ShoppingCartSummary>> replyTo) -> new ShoppingCartCommand.AddItem(itemId, quantity, replyTo),
+                        timeout,
+                        system.scheduler());
+
+        return completionStage.toCompletableFuture();
+    }
+
+    @PostMapping("{cartId}/items/{itemId}")
+    @Operation(summary = "更新商品", description = "更新商品")
+    public CompletableFuture<ShoppingCartSummary> updateItem(@PathVariable("cartId") String cartId, @PathVariable("itemId") String itemId, @RequestParam("quantity") int quantity) {
+        ActorRef<ShoppingCartCommand> shoppingCart = shoppingCarts.get(cartId);
+        CompletionStage<ShoppingCartSummary> completionStage =
+                AskPattern.askWithStatus(
+                        shoppingCart,
+                        (ActorRef<StatusReply<ShoppingCartSummary>> replyTo) -> new ShoppingCartCommand.AdjustItemQuantity(itemId, quantity, replyTo),
+                        timeout,
+                        system.scheduler());
+
+        return completionStage.toCompletableFuture();
+    }
+
+    @DeleteMapping("{cartId}/items/{itemId}")
+    @Operation(summary = "删除商品", description = "删除商品")
+    public CompletableFuture<ShoppingCartSummary> deleteItem(@PathVariable("cartId") String cartId, @PathVariable("itemId") String itemId) {
+        ActorRef<ShoppingCartCommand> shoppingCart = shoppingCarts.get(cartId);
+        CompletionStage<ShoppingCartSummary> completionStage =
+                AskPattern.askWithStatus(
+                        shoppingCart,
+                        (ActorRef<StatusReply<ShoppingCartSummary>> replyTo) -> new ShoppingCartCommand.RemoveItem(itemId, replyTo),
+                        timeout,
+                        system.scheduler());
+
+        return completionStage.toCompletableFuture();
+    }
 
 }
