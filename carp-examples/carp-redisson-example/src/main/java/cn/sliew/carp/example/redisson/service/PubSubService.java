fecea47cc2c044d34f9e4f56ab53a369508bbe62
wangqi <wangqi@xinxuan.net>
2024-10-09 20:04:13 +0800

    feature: update redisson example

---
+++ carp-examples/carp-redisson-example/src/main/java/cn/sliew/carp/example/redisson/service/PubSubService.java
@@ -0,0 +1,21 @@
+package cn.sliew.carp.example.redisson.service;
+
+import org.redisson.api.RReliableTopic;
+import org.redisson.api.RedissonClient;
+import org.springframework.beans.factory.InitializingBean;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class PubSubService implements InitializingBean {
+
+    public static final String RELIABLE_TOPIC_NAME = "reliableTopic";
+
+    @Autowired
+    private RedissonClient client;
+
+    @Override
+    public void afterPropertiesSet() throws Exception {
+        RReliableTopic reliableTopic = client.getReliableTopic(RELIABLE_TOPIC_NAME);
+    }
+}
