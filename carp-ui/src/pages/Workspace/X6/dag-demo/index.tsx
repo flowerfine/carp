ccbf86239d11295cd9e8910a07d61a30c64bd055
wangqi <wangqi@xinxuan.net>
2025-02-15 16:09:32 +0800

    feature: update x6 dag web

--- carp-ui/src/pages/Workspace/X6/dag-demo/index.tsx
+++ carp-ui/src/pages/Workspace/X6/dag-demo/index.tsx
@@ -1,41 +1,39 @@
-import {PageContainer} from "@ant-design/pro-components";
-import {history, useIntl} from "@umijs/max";
-import {Background, Control, ControlEnum, Grid, Minimap, Snapline, XFlow, XFlowGraph} from "@antv/xflow";
+import { XFlow, XFlowGraph, Clipboard, Control } from '@antv/xflow';
+
+import { Dnd } from './dnd/dnd';
 import styles from './index.less';
+import { DAG_EDGE, DAG_CONNECTOR } from './shape';
 
-const DagDemoWeb: React.FC = () => {
-  const intl = useIntl();
-
+const Page = () => {
   return (
-    <PageContainer
-      title={intl.formatMessage({id: 'menu.workspace.x6.dag-demo'})}
-      onBack={() => history.back()}
-    >
-      <XFlow>
-        <div className={styles.page}>
-          <div className={styles.container}>
-            <div className={styles.center}>
-              <div className={styles.graph}>
-                <XFlowGraph zoomable minScale={0.5}/>
-                <Grid type="mesh" options={{color: '#ccc', thickness: 1}}/>
-                <Snapline sharp/>
-                <Background/>
-                <div className={styles.scaleToolbar}>
-                  <Control
-                    items={[ControlEnum.ZoomToOrigin, ControlEnum.ZoomToFit, ControlEnum.ZoomIn, ControlEnum.ZoomTo, ControlEnum.ZoomOut]}
-                    direction={'vertical'}
-                  />
-                </div>
-                <div className={styles.minimap}>
-                  <Minimap/>
-                </div>
+    <XFlow>
+      <div className={styles.page}>
+        <div className={styles.container}>
+          <div className={styles.left}>
+            <div className={styles.leftTop}>算子组件库</div>
+            <Dnd />
+          </div>
+          <div className={styles.center}>
+
+            <div className={styles.graph}>
+              <XFlowGraph
+                pannable
+              />
+
+              
+
+              <Clipboard />
+              <div className={styles.controlTool}>
+                <Control
+                  items={['zoomOut', 'zoomTo', 'zoomIn', 'zoomToFit', 'zoomToOrigin']}
+                />
               </div>
             </div>
           </div>
         </div>
-      </XFlow>
-    </PageContainer>
-  )
-}
+      </div>
+    </XFlow>
+  );
+};
 
-export default DagDemoWeb
+export default Page;
