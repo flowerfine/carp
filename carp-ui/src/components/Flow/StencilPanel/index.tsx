2275c78ad06e821969fefac1818d4c93bc5d1087
wangqi <wangqi@xinxuan.net>
2025-03-12 18:47:05 +0800

    feature: update dag stencil demo

--- carp-ui/src/components/Flow/StencilPanel/index.tsx
+++ carp-ui/src/components/Flow/StencilPanel/index.tsx
@@ -30,8 +30,7 @@ const StencilPanel: React.FC = (props: Stencil.Options) => {
         },
         placeholder: 'Search by shape name',
         notFoundText: 'Not Found',
-        stencilGraphWidth: 300,
-        stencilGraphHeight: 180,
+        stencilGraphHeight: 0,
         collapsable: true,
         groups: [
           {
@@ -51,9 +50,10 @@ const StencilPanel: React.FC = (props: Stencil.Options) => {
           },
         ],
         layoutOptions: {
-          columns: 2,
-          columnWidth: 80,
-          rowHeight: 55,
+          columns: 1,
+          columnWidth: 185,
+          columnHeight: 185,
+          resizeToFit: true
         }
       });
 
@@ -62,10 +62,10 @@ const StencilPanel: React.FC = (props: Stencil.Options) => {
 
       const input = graph.createNode({
         shape: PROCESS_NODE,
-        x: 40,
-        y: 40,
-        width: 80,
-        height: 40,
+        x: 0,
+        y: 0,
+        width: 85,
+        height: 20,
         attrs: commonAttrs,
         data: {
           type: 'INPUT',
@@ -76,10 +76,10 @@ const StencilPanel: React.FC = (props: Stencil.Options) => {
 
       const output = graph.createNode({
         shape: PROCESS_NODE,
-        x: 40,
-        y: 40,
-        width: 80,
-        height: 40,
+        x: 0,
+        y: 0,
+        width: 85,
+        height: 20,
         attrs: commonAttrs,
         data: {
           type: 'OUTPUT',
@@ -88,40 +88,65 @@ const StencilPanel: React.FC = (props: Stencil.Options) => {
         },
       })
 
-      const n2 = graph.createNode({
-        label: 'circle',
-        shape: 'circle',
-        x: 180,
-        y: 40,
-        width: 40,
-        height: 40,
+      const filter = graph.createNode({
+        shape: PROCESS_NODE,
+        x: 0,
+        y: 0,
+        width: 85,
+        height: 20,
         attrs: commonAttrs,
+        data: {
+          type: 'FILTER',
+          name: '数据过滤',
+          status: CellStatus.DEFAULT,
+        },
       })
 
-      const n3 = graph.createNode({
-        shape: 'ellipse',
-        x: 280,
-        y: 40,
-        width: 80,
-        height: 40,
-        label: 'ellipse',
+      const join = graph.createNode({
+        shape: PROCESS_NODE,
+        x: 0,
+        y: 0,
+        width: 85,
+        height: 20,
         attrs: commonAttrs,
+        data: {
+          type: 'JOIN',
+          name: '数据连接',
+          status: CellStatus.DEFAULT,
+        },
       })
 
-      const n4 = graph.createNode({
-        shape: 'path',
-        x: 420,
-        y: 40,
-        width: 40,
-        height: 40,
-        path: 'M24.85,10.126c2.018-4.783,6.628-8.125,11.99-8.125c7.223,0,12.425,6.179,13.079,13.543c0,0,0.353,1.828-0.424,5.119c-1.058,4.482-3.545,8.464-6.898,11.503L24.85,48L7.402,32.165c-3.353-3.038-5.84-7.021-6.898-11.503c-0.777-3.291-0.424-5.119-0.424-5.119C0.734,8.179,5.936,2,13.159,2C18.522,2,22.832,5.343,24.85,10.126z',
+      const union = graph.createNode({
+        shape: PROCESS_NODE,
+        x: 0,
+        y: 0,
+        width: 85,
+        height: 20,
         attrs: commonAttrs,
-        label: 'path',
+        data: {
+          type: 'UNION',
+          name: '数据合并',
+          status: CellStatus.DEFAULT,
+        },
+      })
+
+      const agg = graph.createNode({
+        shape: PROCESS_NODE,
+        x: 0,
+        y: 0,
+        width: 85,
+        height: 20,
+        attrs: commonAttrs,
+        data: {
+          type: 'AGG',
+          name: '数据聚合',
+          status: CellStatus.DEFAULT,
+        },
       })
 
       stencil.load([input], 'source')
       stencil.load([output], 'sink')
-      stencil.load([n2, n3, n4], 'transform')
+      stencil.load([filter, join, union, agg], 'transform')
     }
   }, [graph, props]);
 
