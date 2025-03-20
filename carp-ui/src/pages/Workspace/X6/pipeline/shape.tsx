c0b39a16807cc5e03df221eaec7a3f81e61c2150
wangqi <wangqi@xinxuan.net>
2025-03-20 22:22:18 +0800

    feature: update x6 pipeline demo

--- carp-ui/src/pages/Workspace/X6/pipeline/shape.tsx
+++ carp-ui/src/pages/Workspace/X6/pipeline/shape.tsx
@@ -1,19 +1,73 @@
-import React, {useState} from "react";
+import React from "react";
 import {Graph, Node, register, XFlow} from "@antv/xflow";
-import BasicNode from "@/components/X6/Shape/BasicNode";
+import useStyles from './shapeStyle';
+import {PlusOutlined} from "@ant-design/icons";
+import {Button} from "antd";
+import classnames from "classnames";
+
+const STROKEWIDTH = 2
+const EDGES_LENGTH = 35
+const POINT_R = 6
+const CURVE_R = 10
 
 const PIPELINE_NODE = 'pipeline-node';
 const PIPELINE_EDGE = 'pipeline-edge';
 
 const X6PipelineNode = ({node}: { node: Node }) => {
-  const [openDrawerForm, setOpenDrawerForm] = useState<boolean>(false)
+  const {styles} = useStyles();
+
+  const renderEditButton = () => {
+    return (
+      <g className={styles.editButton}>
+        <circle
+          cx={EDGES_LENGTH}
+          cy={POINT_R}
+          r="10"
+          stroke="#d8dee5"
+          strokeDasharray="3,3"
+          fill="white"
+          strokeWidth={STROKEWIDTH}
+        />
+        <line
+          x1={EDGES_LENGTH - 5}
+          x2={EDGES_LENGTH + 5}
+          y1={POINT_R}
+          y2={POINT_R}
+          stroke="black"
+        />
+        <line
+          x1={EDGES_LENGTH}
+          x2={EDGES_LENGTH}
+          y1={POINT_R - 5}
+          y2={POINT_R + 5}
+          stroke="black"
+        />
+      </g>
+    )
+  }
+
+
+  const renderBoldLines = () => {
+    // return (
+    //   <>
+    //     {this.renderLines(heights[index], 'left')}
+    //     {this.renderLines(heights[index + 1], 'right')}
+    //   </>
+    // )
+  }
+
 
   return (
     <XFlow>
-      <div onDoubleClick={() => setOpenDrawerForm(true)}>
-        <BasicNode
-          node={node}
+      <div className={classnames(styles.connectorLine)}>
+        <div className={classnames(styles.connectorDot)}></div>
+        <div className={classnames(styles.connectorLineSegment)}></div>
+        <Button
+          className={classnames(styles.connectorButton)}
+          type="primary" shape="circle" icon={<PlusOutlined />} size="small"
         />
+        <div className={classnames(styles.connectorLineSegment)}></div>
+        <div className={classnames(styles.connectorDot)}></div>
       </div>
     </XFlow>
   );
