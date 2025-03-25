bfa8daef34b154024308a6522b63d53e96cae7da
wangqi <wangqi@xinxuan.net>
2025-03-25 13:26:11 +0800

    feature: update simple workflow

--- carp-modules/carp-module-workflow/carp-module-workflow-simple/src/main/java/cn/sliew/carp/module/workflow/simple/tasks/BaseLocalStepTask.java
+++ carp-modules/carp-module-workflow/carp-module-workflow-simple/src/main/java/cn/sliew/carp/module/workflow/simple/tasks/BaseLocalStepTask.java
@@ -17,11 +17,52 @@
  */
 package cn.sliew.carp.module.workflow.simple.tasks;
 
+import cn.hutool.extra.spring.SpringUtil;
+import cn.sliew.carp.framework.dag.service.DagInstanceService;
+import cn.sliew.carp.framework.dag.service.dto.DagInstanceDTO;
 import cn.sliew.carp.module.workflow.simple.tasks.domain.WorkflowTaskRunRet;
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.node.ObjectNode;
 
 public abstract class BaseLocalStepTask extends BaseLocalTask {
 
+    public Long workflowInstanceId;
+    public Long workflowStepInstanceId;
+    public Long workflowStepDefinitionId;
+
+    // 如果是 sub-workflow，则为父 workflow 中的 step instance id
+    public Long parentWorkflowStepInstanceId;
+
+    // 全局参数
+    public ObjectNode globalParams;
+    // 清空全局参数
+    public Boolean isDeleteParams = false;
+
+    // 输入的执行参数，根据inputParams转换过来的
+    public JsonNode params;
+
+    // 全局变量
+    public JsonNode globalVariable;
+    // 最新获取全局变量的时间
+    public Long lastGlobalVariableTimestamp;
+
+    // 上游节点执行的结果，包括result、output
+    public JsonNode globalResult;
+
+    private Long retryTimes = 0L;
+
     public abstract WorkflowTaskRunRet run() throws Exception;
+
+    public void freshGlobalVariable() {
+        if (System.currentTimeMillis() / 1000 - lastGlobalVariableTimestamp < 1) {
+            return;
+        }
+
+        DagInstanceService dagInstanceService = SpringUtil.getBean(DagInstanceService.class);
+        DagInstanceDTO dagInstanceDTO = dagInstanceService.get(workflowInstanceId);
+        globalVariable = dagInstanceDTO.getInputs();
+        lastGlobalVariableTimestamp = System.currentTimeMillis() / 1000;
+    }
 
     public void stop() {
 
