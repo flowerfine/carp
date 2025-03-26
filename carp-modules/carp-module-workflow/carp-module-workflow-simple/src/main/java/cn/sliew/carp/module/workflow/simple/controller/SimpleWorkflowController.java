1d1a27c140e31627ad368d7363cd3be3ac011dfe
wangqi <wangqi@xinxuan.net>
2025-03-26 10:52:05 +0800

    feature: upgrade carp-spring-boot-parent

--- carp-modules/carp-module-workflow/carp-module-workflow-simple/src/main/java/cn/sliew/carp/module/workflow/simple/controller/SimpleWorkflowController.java
+++ carp-modules/carp-module-workflow/carp-module-workflow-simple/src/main/java/cn/sliew/carp/module/workflow/simple/controller/SimpleWorkflowController.java
@@ -18,23 +18,19 @@
 package cn.sliew.carp.module.workflow.simple.controller;
 
 import cn.sliew.carp.framework.common.util.UUIDUtil;
-import cn.sliew.carp.framework.dag.x6.dnd.X6GraphDTO;
-import cn.sliew.carp.framework.log.annotation.WebLog;
+import cn.sliew.carp.framework.log.web.annotation.WebLog;
 import cn.sliew.carp.framework.web.response.ApiResponseWrapper;
-import cn.sliew.carp.module.workflow.api.service.ServerlessWorkflowService;
-import cn.sliew.carp.module.workflow.api.service.dto.dnd.DndGroupDTO;
-import cn.sliew.carp.module.workflow.api.service.param.ServerlessWorkflowExecuteParam;
 import cn.sliew.carp.module.workflow.simple.api.WorkflowDefinitionApi;
 import cn.sliew.carp.module.workflow.simple.api.request.WorkflowDefinitionRequest;
 import cn.sliew.carp.module.workflow.simple.api.request.WorkflowLinkDefinition;
 import cn.sliew.carp.module.workflow.simple.api.request.WorkflowStepDefinition;
 import cn.sliew.carp.module.workflow.simple.enums.WorkflowStepType;
-import com.fasterxml.jackson.databind.JsonNode;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
-import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
