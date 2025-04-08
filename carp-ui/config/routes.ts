feaec7ea17e9e95f531d690ed5b484def4df724e
wangqi <wangqi@xinxuan.net>
2025-04-08 10:12:12 +0800

    feature: update flowchart

--- carp-ui/config/routes.ts
+++ carp-ui/config/routes.ts
@@ -246,6 +246,12 @@ export default [
           {
             path: '/workspace/serverless-workflow/instance/detail',
             component: './Workspace/ServerlessWorkflow/Instance/Detail',
+          },
+          {
+            path: '/workspace/serverless-workflow/flowchart',
+            name: 'flowchart',
+            icon: 'profile',
+            component: './Workspace/ServerlessWorkflow/FlowChart',
           }
         ]
       },
