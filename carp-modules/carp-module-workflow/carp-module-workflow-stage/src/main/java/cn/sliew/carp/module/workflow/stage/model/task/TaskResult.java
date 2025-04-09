8eda23fa8ed8cdd651876b73c920c5bfd81c1067
wangqi <wangqi@xinxuan.net>
2025-04-09 09:41:37 +0800

    prepare release

--- carp-modules/carp-module-workflow/carp-module-workflow-stage/src/main/java/cn/sliew/carp/module/workflow/stage/model/task/TaskResult.java
+++ carp-modules/carp-module-workflow/carp-module-workflow-stage/src/main/java/cn/sliew/carp/module/workflow/stage/model/task/TaskResult.java
@@ -18,14 +18,11 @@
 package cn.sliew.carp.module.workflow.stage.model.task;
 
 import cn.sliew.carp.module.workflow.domain.ExecutionStatus;
-import lombok.Builder;
-import lombok.Data;
-import lombok.Singular;
+import lombok.Getter;
 
-import java.util.Map;
+import java.util.*;
 
-@Data
-@Builder
+@Getter
 @SuppressWarnings("FallThrough")
 public final class TaskResult {
 
@@ -38,14 +35,18 @@ public final class TaskResult {
     /**
      * Step-scoped data.
      */
-    @Singular("context")
     private final Map<String, ?> context;
 
     /**
      * Workflow-scoped data.
      */
-    @Singular("output")
     private final Map<String, ?> outputs;
+
+    TaskResult(ExecutionStatus status, Map<String, ?> context, Map<String, ?> outputs) {
+        this.status = status;
+        this.context = context;
+        this.outputs = outputs;
+    }
 
     public static TaskResult ofStatus(ExecutionStatus status) {
         return TaskResult.builder(status).build();
@@ -53,5 +54,137 @@ public final class TaskResult {
 
     public static TaskResultBuilder builder(ExecutionStatus status) {
         return new TaskResultBuilder().status(status);
+    }
+
+    public static class TaskResultBuilder {
+        private ExecutionStatus status;
+        private ArrayList<String> context$key;
+        private ArrayList<Object> context$value;
+        private ArrayList<String> outputs$key;
+        private ArrayList<Object> outputs$value;
+
+        TaskResultBuilder() {
+        }
+
+        public TaskResultBuilder status(ExecutionStatus status) {
+            this.status = status;
+            return this;
+        }
+
+        public TaskResultBuilder context(String contextKey, Object contextValue) {
+            if (this.context$key == null) {
+                this.context$key = new ArrayList();
+                this.context$value = new ArrayList();
+            }
+
+            this.context$key.add(contextKey);
+            this.context$value.add(contextValue);
+            return this;
+        }
+
+        public TaskResultBuilder context(Map<? extends String, ?> context) {
+            if (context == null) {
+                throw new NullPointerException("context cannot be null");
+            } else {
+                if (this.context$key == null) {
+                    this.context$key = new ArrayList();
+                    this.context$value = new ArrayList();
+                }
+
+                for (Map.Entry<? extends String, ?> entry : context.entrySet()) {
+                    this.context$key.add(entry.getKey());
+                    this.context$value.add(entry.getValue());
+                }
+
+                return this;
+            }
+        }
+
+        public TaskResultBuilder clearContext() {
+            if (this.context$key != null) {
+                this.context$key.clear();
+                this.context$value.clear();
+            }
+
+            return this;
+        }
+
+        public TaskResultBuilder output(String outputKey, Object outputValue) {
+            if (this.outputs$key == null) {
+                this.outputs$key = new ArrayList();
+                this.outputs$value = new ArrayList();
+            }
+
+            this.outputs$key.add(outputKey);
+            this.outputs$value.add(outputValue);
+            return this;
+        }
+
+        public TaskResultBuilder outputs(Map<? extends String, ?> outputs) {
+            if (outputs == null) {
+                throw new NullPointerException("outputs cannot be null");
+            } else {
+                if (this.outputs$key == null) {
+                    this.outputs$key = new ArrayList();
+                    this.outputs$value = new ArrayList();
+                }
+
+                for (Map.Entry<? extends String, ?> entry : outputs.entrySet()) {
+                    this.outputs$key.add(entry.getKey());
+                    this.outputs$value.add(entry.getValue());
+                }
+
+                return this;
+            }
+        }
+
+        public TaskResultBuilder clearOutputs() {
+            if (this.outputs$key != null) {
+                this.outputs$key.clear();
+                this.outputs$value.clear();
+            }
+
+            return this;
+        }
+
+        public TaskResult build() {
+            Map context;
+            switch (this.context$key == null ? 0 : this.context$key.size()) {
+                case 0:
+                    context = Collections.emptyMap();
+                    break;
+                case 1:
+                    context = Collections.singletonMap(this.context$key.get(0), this.context$value.get(0));
+                    break;
+                default:
+                    Map<String, Object> internalContext = new LinkedHashMap(this.context$key.size() < 1073741824 ? 1 + this.context$key.size() + (this.context$key.size() - 3) / 3 : Integer.MAX_VALUE);
+
+                    for(int $i = 0; $i < this.context$key.size(); ++$i) {
+                        internalContext.put(this.context$key.get($i), this.context$value.get($i));
+                    }
+
+                    context = Collections.unmodifiableMap(internalContext);
+            }
+
+            Map outputs;
+            switch (this.outputs$key == null ? 0 : this.outputs$key.size()) {
+                case 0:
+                    outputs = Collections.emptyMap();
+                    break;
+                case 1:
+                    outputs = Collections.singletonMap(this.outputs$key.get(0), this.outputs$value.get(0));
+                    break;
+                default:
+                    Map<String, Object> internalOutputs = new LinkedHashMap(this.outputs$key.size() < 1073741824 ? 1 + this.outputs$key.size() + (this.outputs$key.size() - 3) / 3 : Integer.MAX_VALUE);
+
+                    for(int $i = 0; $i < this.outputs$key.size(); ++$i) {
+                        internalOutputs.put(this.outputs$key.get($i), this.outputs$value.get($i));
+                    }
+
+                    outputs = Collections.unmodifiableMap(internalOutputs);
+            }
+
+            return new TaskResult(this.status, context, outputs);
+        }
     }
 }
