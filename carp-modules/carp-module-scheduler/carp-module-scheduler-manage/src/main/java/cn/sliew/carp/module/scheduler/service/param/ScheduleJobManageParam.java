a57a96e744bdd22c7324accde6ada6792455edc4
wangqi <wangqi@xinxuan.net>
2025-07-09 10:34:39 +0800

    fix: validate annotation error

--- carp-modules/carp-module-scheduler/carp-module-scheduler-manage/src/main/java/cn/sliew/carp/module/scheduler/service/param/ScheduleJobManageParam.java
+++ carp-modules/carp-module-scheduler/carp-module-scheduler-manage/src/main/java/cn/sliew/carp/module/scheduler/service/param/ScheduleJobManageParam.java
@@ -18,13 +18,13 @@
 package cn.sliew.carp.module.scheduler.service.param;
 
 import io.swagger.v3.oas.annotations.media.Schema;
-import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 import lombok.Data;
 
 @Data
 public class ScheduleJobManageParam {
 
-    @NotBlank
+    @NotNull
     @Schema(description = "任务实例 ID")
     private Long jobInstanceId;
 }
