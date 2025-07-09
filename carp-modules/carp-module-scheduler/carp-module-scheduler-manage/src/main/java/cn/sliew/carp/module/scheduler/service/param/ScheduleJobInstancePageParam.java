b4319243e5e28888a0b21d91a95c8d3203f39d69
wangqi <wangqi@xinxuan.net>
2025-07-09 10:26:04 +0800

    fix: remove useless imports

--- carp-modules/carp-module-scheduler/carp-module-scheduler-manage/src/main/java/cn/sliew/carp/module/scheduler/service/param/ScheduleJobInstancePageParam.java
+++ carp-modules/carp-module-scheduler/carp-module-scheduler-manage/src/main/java/cn/sliew/carp/module/scheduler/service/param/ScheduleJobInstancePageParam.java
@@ -22,7 +22,6 @@ import cn.sliew.carp.framework.common.model.BasePageParam;
 import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.constraints.NotNull;
 import lombok.Data;
-import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 import lombok.experimental.SuperBuilder;
 import lombok.extern.jackson.Jacksonized;
